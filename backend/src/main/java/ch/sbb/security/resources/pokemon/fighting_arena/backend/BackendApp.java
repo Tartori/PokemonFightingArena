c8caed84ec96afdeeaee9d8fa839e83b44ecab48
u233385 <u233385@sbb.ch>
2020-10-19 16:45:18 +0200

    chore: initial setup

---
+++ backend/src/main/java/ch/sbb/security/resources/pokemon/fighting_arena/backend/BackendApp.java
@@ -0,0 +1,12 @@
+package ch.sbb.security.resources.pokemon.fighting_arena.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApp.class, args);
+    }
+}
